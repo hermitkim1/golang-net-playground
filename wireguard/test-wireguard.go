6bee12bc41566a6ab78028ae474424b440530eaf
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2022-01-17 15:47:41 +0900

    Test to create TUN device with wireguard-go

---
+++ wireguard/test-wireguard.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"strconv"
+	"syscall"
+
+	"golang.zx2c4.com/wireguard/tun"
+)
+
+const (
+	ExitSetupSuccess = 0
+	ExitSetupFailed  = 1
+)
+
+const (
+	ENV_WG_TUN_FD             = "WG_TUN_FD"
+	ENV_WG_UAPI_FD            = "WG_UAPI_FD"
+	ENV_WG_PROCESS_FOREGROUND = "WG_PROCESS_FOREGROUND"
+)
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-f/--foreground] INTERFACE-NAME\n", os.Args[0])
+}
+
+func warning() {
+	switch runtime.GOOS {
+	case "linux", "freebsd", "openbsd":
+		if os.Getenv(ENV_WG_PROCESS_FOREGROUND) == "1" {
+			return
+		}
+	default:
+		return
+	}
+
+	fmt.Fprintln(os.Stderr, "┌──────────────────────────────────────────────────────┐")
+	fmt.Fprintln(os.Stderr, "│                                                      │")
+	fmt.Fprintln(os.Stderr, "│   Running wireguard-go is not required because this  │")
+	fmt.Fprintln(os.Stderr, "│   kernel has first class support for WireGuard. For  │")
+	fmt.Fprintln(os.Stderr, "│   information on installing the kernel module,       │")
+	fmt.Fprintln(os.Stderr, "│   please visit:                                      │")
+	fmt.Fprintln(os.Stderr, "│         https://www.wireguard.com/install/           │")
+	fmt.Fprintln(os.Stderr, "│                                                      │")
+	fmt.Fprintln(os.Stderr, "└──────────────────────────────────────────────────────┘")
+}
+
+func main() {
+	// if len(os.Args) == 2 && os.Args[1] == "--version" {
+	// 	fmt.Printf("wireguard-go v%s\n\nUserspace WireGuard daemon for %s-%s.\nInformation available at https://www.wireguard.com.\nCopyright (C) Jason A. Donenfeld <Jason@zx2c4.com>.\n", Version, runtime.GOOS, runtime.GOARCH)
+	// 	return
+	// }
+
+	// warning()
+
+	var foreground bool
+	var interfaceName string
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		printUsage()
+		return
+	}
+
+	switch os.Args[1] {
+
+	case "-f", "--foreground":
+		foreground = true
+		if len(os.Args) != 3 {
+			printUsage()
+			return
+		}
+		interfaceName = os.Args[2]
+
+	default:
+		foreground = false
+		if len(os.Args) != 2 {
+			printUsage()
+			return
+		}
+		interfaceName = os.Args[1]
+	}
+
+	if !foreground {
+		foreground = os.Getenv(ENV_WG_PROCESS_FOREGROUND) == "1"
+	}
+
+	// get log level (default: info)
+
+	// logLevel := func() int {
+	// 	switch os.Getenv("LOG_LEVEL") {
+	// 	case "verbose", "debug":
+	// 		return device.LogLevelVerbose
+	// 	case "error":
+	// 		return device.LogLevelError
+	// 	case "silent":
+	// 		return device.LogLevelSilent
+	// 	}
+	// 	return device.LogLevelError
+	// }()
+
+	// open TUN device (or use supplied fd)
+
+	tun, err := func() (tun.Device, error) {
+		tunFdStr := os.Getenv(ENV_WG_TUN_FD)
+		if tunFdStr == "" {
+			return tun.CreateTUN(interfaceName, 1420)
+		}
+
+		// construct tun device from supplied fd
+
+		fd, err := strconv.ParseUint(tunFdStr, 10, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		err = syscall.SetNonblock(int(fd), true)
+		if err != nil {
+			return nil, err
+		}
+
+		file := os.NewFile(uintptr(fd), "")
+		return tun.CreateTUNFromFile(file, 1420)
+	}()
+
+	if err == nil {
+		realInterfaceName, err2 := tun.Name()
+		if err2 == nil {
+			interfaceName = realInterfaceName
+		}
+	}
+
+	// Set interface parameters
+	runIP("link", "set", "dev", interfaceName, "mtu", strconv.Itoa(1420))
+	runIP("addr", "add", "192.168.10.1/28", "dev", interfaceName)
+	runIP("link", "set", "dev", interfaceName, "up")
+
+	// logger := device.NewLogger(
+	// 	logLevel,
+	// 	fmt.Sprintf("(%s) ", interfaceName),
+	// )
+
+	errs := make(chan error)
+	term := make(chan os.Signal, 1)
+
+	// wait for program to terminate
+
+	signal.Notify(term, syscall.SIGTERM)
+	signal.Notify(term, os.Interrupt)
+
+	select {
+	case <-term:
+	case <-errs:
+		// case <-device.Wait():
+	}
+
+	fmt.Println("Shutting down")
+}
+
+func runIP(args ...string) {
+	fmt.Println("Start.........")
+
+	fmt.Println(args)
+
+	cmd := exec.Command("/sbin/ip", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if nil != err {
+		fmt.Println("Error running /sbin/ip:", err)
+	}
+
+}
